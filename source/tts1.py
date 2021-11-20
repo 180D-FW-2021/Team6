1e823155b0ce1908174af5d5ea71652e13f168ec
Sylvia <sylzheng@ucla.edu>
2021-11-20 02:52:05 -0800

    Use pygame for audio playback of tts output

--- source/tts1.py
+++ source/tts1.py
@@ -3,7 +3,6 @@ import speech_recognition as sr
 import time
 import sys
 import pygame
-import threading
 
 # TODO:
 #       add reading of ocr text result
@@ -11,16 +10,40 @@ import threading
 #       for testing purposes, pass in sample text as command line arg
 #           i.e. `python tts1.py sampletext.txt`
 
-sampletextfile = open(sys.argv[1], "r")
-sampletext = sampletextfile.read()
+# references:
+#		https://stackoverflow.com/questions/65730317/how-to-pause-resume-and-stop-pyttsx3-from-speaking
+#		Using this, we will finish processing the text to speech before beginning reading
 
+sampletextfile = open(sys.argv[1], "r")
+sampleText = sampletextfile.read()
+
+pygame.mixer.init()
 engine = pyttsx3.init()
 r = sr.Recognizer()
 m = sr.Microphone()
-with m as source: r.adjust_for_ambient_noise(source)
 
-engine.say(sampletext)
-engine.runAndWait()
+already_processed = False
+
+def read():
+	outfile = "temp.wav"
+	if not already_processed:
+		engine.save_to_file(sampleText, outfile)
+		engine.runAndWait()
+	pygame.mixer.music.load(outfile)
+	pygame.mixer.music.play()
+
+def stop():
+    pygame.mixer.music.stop()
+
+def pause():
+    pygame.mixer.music.pause()
+
+def unpause():
+    pygame.mixer.music.unpause()
+
+with m as source: 
+	r.adjust_for_ambient_noise(source)
+
 while (1):
     with sr.Microphone() as source:
         print("say something!")
@@ -30,26 +53,37 @@ while (1):
         speech = r.recognize_google(audio)
         print("You said: " + speech)
 
+        speech = speech.split()[0]
+        print("Command given: " speech)
+
         if speech == "start":
             phrase = "starting text reading"
-            engine.say(phrase)
             print(phrase)
+            read()
         elif speech == "stop":
             phrase = "stopping text reading"
-            engine.say(phrase)
             print(phrase)
-        elif speech == "speed up":
-            phrase = "speeding up"
-            engine.say(phrase)
+            pause()
+        elif speech == "pause":
+            phrase = "pausing text reading"
             print(phrase)
-        elif speech == ("slow down"):
-            phrase = "slowing down"
-            engine.say(phrase)
+            pause()
+        elif speech == "play":
+            phrase = "resuming text reading"
             print(phrase)
-
-        elif speech == "hello":
-            engine.say("Hi there. Welcome to Read Me.")
-        engine.runAndWait()
+            unpause()
+	    # TODO speeding up/down currently not implemented, 
+	    #      complications with the time required to resample the wav file
+            '''
+            elif speech == "speed up":
+                phrase = "speeding up"
+                engine.say(phrase)
+                print(phrase)
+            elif speech == ("slow down"):
+                phrase = "slowing down"
+                engine.say(phrase)
+                print(phrase)
+            '''
 
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
