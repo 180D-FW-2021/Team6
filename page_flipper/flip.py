45e25224fa557c490adae0b4474444c72eb9a682
sylzheng <sylzheng@ucla.edu>
2021-12-05 21:17:14 +0000

    added test script to check that motors are working properly when given 'f' and 'b' console inputs in place of imu data

--- page_flipper/flip.py
+++ page_flipper/flip.py
@@ -1,43 +1,51 @@
 from gpiozero import Motor, Servo, LED
 from time import sleep
+from rpi_hardware_pwm import HardwarePWM
+
+servo=None
+motorF=None
+led=None
+left=True
+right=False
 
 # pageturn_right / backwards hardware is not yet implemented
 # TODO: replace sleep with polling or interrupts
 
 def pageturn_backwards():
-	print('turning backwards not yet implemented')
-	return
-	# motorB.forward()
-	# sleep(300)
-	# motorB.stop()
-	# servo.min()
-	# sleep(100)
+    print('turning backwards not yet implemented')
+    return
+    # motorB.forward()
+    # sleep(300)
+    # motorB.stop()
+    # servo.min()
+    # sleep(100)
 
 def pageturn_forward():
-	motorF.forward()
-	sleep(300)
-	motorF.stop()
-	servo.max()
-	sleep(100)
-	servo.min()
+    motorF.forward(0.5)
+    sleep(0.3)
+    motorF.stop()
+    servo.change_duty_cycle(100)
+    sleep(0.1)
+    servo.change_duty_cycle(0)
+    print('turning page forward')
 
 def init():
-	global servo, motor, led
-	servo = Servo(17)
-	motorF = Motor(18, 27)
-	led = LED(7)
-	# motorB = Motor(5, 6)
+    global servo, motorF, led
+    servo = HardwarePWM(0, hz=60)
+    servo.start(0)
+    motorF = Motor(17, 27)
+    led = LED(7)
+    print( 'hardware initialized' )
+    # motorB = Motor(5, 6)
 
 def main():
-	led.on()
-	while(1):
-		if (left):
-			pageturn_forward()
-	
-		if (right):
-			pageturn_backwards()
+    global servo, motorF, led, left, right
+    init()
+    led.on()
+    if (left):
+        pageturn_forward()
 
 
 if __name__=='__main__':
-	main()
+        main()
 
