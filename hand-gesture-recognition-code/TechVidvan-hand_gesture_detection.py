eac29fd547d2802221c7749faf0a9f54503cd487
olivialoh77 <olivialoh77@gmail.com>
2021-12-06 10:46:44 -0800

    concurrent processing

--- hand-gesture-recognition-code/TechVidvan-hand_gesture_detection.py
+++ hand-gesture-recognition-code/TechVidvan-hand_gesture_detection.py
@@ -26,23 +26,26 @@ print(classNames)
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
 
+counter = 0 
+pose = "" 
+
 while True:
     # Read each frame from the webcam
     _, frame = cap.read()
-
+	
     x, y, c = frame.shape
-
+	
     # Flip the frame vertically
     frame = cv2.flip(frame, 1)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+	
     # Get hand landmark prediction
     result = hands.process(framergb)
-
+	
     # print(result)
     
     className = ''
-
+	
     # post process the result
     if result.multi_hand_landmarks:
         landmarks = []
@@ -56,19 +59,31 @@ while True:
 
             # Drawing landmarks on frames
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-
+            
             # Predict gesture
             prediction = model.predict([landmarks])
             # print(prediction)
             classID = np.argmax(prediction)
             className = classNames[classID]
-
+    
     # show the prediction on the frame
-    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                   1, (0,0,255), 2, cv2.LINE_AA)
-
+    if className == 'stop' or className == 'thumbs up': 
+        cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+    
     # Show the final output
-    cv2.imshow("Output", frame) 
+    cv2.imshow("Output", frame)
+    
+    if counter > 5: 
+        if className == 'stop': 
+            print("stop") 
+        elif className == 'thumbs up':
+            print("start")
+        counter = 0
+    if pose == className: 
+        counter = counter + 1 
+    
+    pose = className 
+            
 
     if cv2.waitKey(1) == ord('q'):
         break
