36f6f6a5ec4c68b470b68c87ff30ab0505625514
Khoa Quach <49701587+khcsce@users.noreply.github.com>
2022-01-26 16:42:12 -0800

    updated with possible pip installs needed

--- Executable_Shell_script/run.sh
+++ Executable_Shell_script/run.sh
@@ -11,6 +11,17 @@ then
     pip install pytesseract 
     pip install numpy
     pip install opencv-python
+    '''  
+    other possible ones:
+    pip install opencv-contrib-python
+    pip install pyttsx3
+    pip install speechRecognition
+    pip install pygame
+    pip install numpy
+    pip install mediapipe
+    pip install --upgrade tensorflow
+    pip install paho-mqtt
+    '''
 
 fi
 
