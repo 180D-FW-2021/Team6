66309a04520f265f9ff14a93f8a46400256869f9
Joshafutt <joshafuttcomp23@ucla.edu>
2022-01-30 13:24:01 -0800

    Added shell script for image and updated mqtt code for images

--- Communications/Image_subsriber.py
+++ Communications/Image_subsriber.py
@@ -1,5 +1,4 @@
 import paho.mqtt.client as mqtt
-import numpy as np
 
 # 0. define callbacks - functions that run when events happen.
 # The callback for when the client receives a CONNACK response from the server.
@@ -10,7 +9,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    # client.subscribe("ece180d/test")
+    client.subscribe("ece180d/image", qos=1)
 
 # The callback of the client when it disconnects.
 
@@ -22,12 +21,16 @@ def on_disconnect(client, userdata, rc):
         print('Expected Disconnect')
 
 # The default message callback.
-# (won’t be used if only publishing, but can still exist)
+# (you can create separate callbacks per subscribed topic)
 
 
 def on_message(client, userdata, message):
-    print('Received message: "' + str(message.payload) + '" on topic "' +
-          message.topic + '" with QoS ' + str(message.qos))
+    f = open('image1.jpg', 'w')
+    f.write(message.payload)
+    f.close()
+    print('image received')
+    # print('Received message: "' + str(message.payload) + '"on topic "' +
+    # message.topic + '" with QoS ' + str(message.qos))
 
 
 # 1. create a client instance.
@@ -35,24 +38,26 @@ client = mqtt.Client()
 # add additional client options (security, certifications, etc.)
 # many default options should be good to start off.
 # add callbacks to client.
+
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_message = on_message
 
-# 2. connect to a broker using one of the connect*() functions.
+# 2. connect to a broker using one of the connect*() functions.5
+
 client.connect_async('mqtt.eclipseprojects.io')
+# client.connect("mqtt.eclipse.org")
 
 # 3. call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
+# client.loop_forever()
 
-# 4. use subscribe() to subscribe to a topic and receive messages.
+while True:
+    pass
+# use subscribe() to subscribe to a topic and receive messages.
 
-# 5. use publish() to publish messages to the broker.
-# payload must be a string, bytearray, int, float or None.
-text = 'My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'
+# use publish() to publish messages to the broker.
 
-client.publish('ece180d/text', text, qos=1)
-
-# 6. use disconnect() to disconnect from the broker.
+# use disconnect() to disconnect from the broker.
 client.loop_stop()
 client.disconnect()
